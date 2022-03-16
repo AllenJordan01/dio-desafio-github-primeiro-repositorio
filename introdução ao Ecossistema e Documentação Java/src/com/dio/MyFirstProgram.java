d233906ee455e4d3909e793446a2fbf12595cd8f
Allen Jordan <allenjordan65@gmail.com>
2022-03-16 17:09:55 -0300

    criação do arquivo MyfirstProgram.java

---
+++ introdução ao Ecossistema e Documentação Java/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,10 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        final Order order = new Order("code3642");
+        System.out.println(order);
+    }
+}
